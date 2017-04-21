fc6b220cd401405a33002a94f334273909075a25
Daniel Zamorano <dzamorano@tacitknowledge.com>
2017-04-21 09:08:27 -0500

    Updating docs.

--- server.js
+++ server.js
@@ -8,36 +8,42 @@ var PORT = process.env.PORT || 3000;
 
 var todos = [
   {
-    id: 1,
-    body: 'Create a ToDo App.',
+    id: 83515750,
+    description: 'Create a ToDo App.',
     completed: false
   },
   {
-    id: 2,
-    body: 'Consume ToDo API.',
+    id: 51839147,
+    description: 'Consume ToDo API.',
     completed: false
   },
   {
-    id: 3,
-    body: 'Use the frameworks, tools and resources that you need.',
+    id: 71862996,
+    description: 'Use the frameworks, tools and resources that you need.',
     completed: false
   },
   {
-    id: 4,
-    body: 'Follow proposed design.',
+    id: 95484409,
+    description: 'Follow proposed design.',
     completed: false
   },
   {
-    id: 5,
-    body: 'Ask for any question you have regarding this.',
+    id: 40016268,
+    description: 'Ask for any question you have regarding this.',
     completed: false
   },
   {
-    id: 6,
-    body: 'Use best practices, design patterns and the best code styles.',
+    id: 8370263,
+    description: 'Use best practices, design patterns and the best code styles.',
     completed: false
   }
 ];
+
+function randomInt () {
+  var low = 0;
+  var high = 100000000;
+  return Math.floor(Math.random() * (high - low) + low);
+}
 
 var todoNextId = 1;
 
@@ -62,6 +68,19 @@ app.get('/api/todos/:id', function (req, res) {
   res.status(404).send();
 });
 
+app.put('/api/todos/:id', function (req, res) {
+  var todoId = parseInt(req.params.id);
+  var matchedTodo = _.findWhere(todos, {id : todoId});
+  var body = _.pick(req.body, 'description', 'completed');
+  var updated = _.extend(matchedTodo, body);
+
+  if (!req.params.id || !_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+
+  res.send(updated);
+});
+
 app.post('/api/todos', function (req, res) {
   var body = _.pick(req.body, 'description', 'completed');
 
@@ -71,7 +90,7 @@ app.post('/api/todos', function (req, res) {
 
   body.description = body.description.trim();
 
-  body.id = todoNextId++;
+  body.id = randomInt();
   todos.push(body);
 
   res.json(body);
